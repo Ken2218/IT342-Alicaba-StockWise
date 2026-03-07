356e28817d859b01a68c870bee16d55c6990df96
LAB13_USER ON WS21 <L13Y17W21@LabHCI.net>
2026-03-07 15:23:10 +0800

    Initial commit of backend project setup

---
+++ stockwise/src/main/java/edu/cit/alicaba/stockwise/StockwiseApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.alicaba.stockwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockwiseApplication.class, args);
+	}
+
+}
---
+++ stockwise/src/test/java/edu/cit/alicaba/stockwise/StockwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.alicaba.stockwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
